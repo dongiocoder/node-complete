41de75cef8eb55290def1a57e2c84e15ebfdc409
17927483 <giovannitosc@gmail.com>
2019-10-03 15:15:04 +0200

    Added Express

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-const http = require('http');
+const express = require('express');
 
-const routes = require('./routes');
+const app = express();
 
-console.log(routes.someText);
+app.use('/add-product', (req, res, next) => {
+    console.log('In another middleware')
+    res.send('<h1>The add product page</h1>');
+})
 
-const server = http.createServer(routes.handler);
+app.use('/', (req, res, next) => {
+    console.log('In another middleware')
+    res.send('<h1> Hello From Express</h1>');
+})
 
-server.listen(3000);
+app.listen(3000);
