7ff7348bdf01a6a6690647f15b92de8cd4708931
17927483 <giovannitosc@gmail.com>
2019-10-04 12:19:30 +0200

    to the beginning of section 6 - all working

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.use('/add-product', (req, res, next) => {
-    console.log('In another middleware')
-    res.send('<h1>The add product page</h1>');
-})
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', (req, res, next) => {
-    console.log('In another middleware')
-    res.send('<h1> Hello From Express</h1>');
-})
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
 app.listen(3000);
